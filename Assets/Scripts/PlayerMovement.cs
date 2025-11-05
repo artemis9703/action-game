c434e402ff6bb7169ccae34d2d3add80cdac553c
Art3mis <pyatt.eve@gmail.com>
2025-11-05 13:22:34 -0800

    animationnnnnnnnnnnnnnnn

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public int facingDirection = 1;
+
     public Rigidbody2D rb;
+    public Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,12 +14,26 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    // Update is called 50 times per frame
+    // fixed update is called 50 times per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if(horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
