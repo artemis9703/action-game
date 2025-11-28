b3e51459415ddeca35552c4e7af6dd4b0d16350b
Art3mis <pyatt.eve@gmail.com>
2025-11-27 23:07:26 -0800

    npcs started

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("Persistent Objects")]
+    public GameObject[] persistentObjects;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            CleanUpAndDestroy();
+            return;
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            MarkPersistentObjects();
+        }
+    }
+
+    private void MarkPersistentObjects()
+    {
+        foreach (GameObject obj in persistentObjects)
+        {
+            if (obj != null)
+            {
+                DontDestroyOnLoad(obj);
+            }
+        }
+    }
+
+    private void CleanUpAndDestroy()
+    {
+        foreach (GameObject obj in persistentObjects)
+        {
+            Destroy(obj);
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneChanger : MonoBehaviour
 {
     public string sceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = 0.5f;
+    public Vector2 newPlayerPosition;
+    private Transform player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +20,22 @@ public class SceneChanger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.transform;
+            fadeAnim.Play("fadeToWhite");
+            StartCoroutine(DelayFade());
+        }
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        player.position = newPlayerPosition;
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
